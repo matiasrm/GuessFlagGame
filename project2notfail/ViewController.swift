13efe83943254ebbc6cef3532c5fa4c991808fdf
Matias Vidoni <matiasrmontenegro@gmail.com>
2020-11-12 14:16:45 -0300

    Initial Commit

---
+++ project2notfail/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project2notfail
+//
+//  Created by Mati Montenegro on 12/11/2020.
+//  Copyright © 2020 Vidoni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
